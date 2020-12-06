875d73919a23ad1b64ad571f11eaec74fd8bba7b
PW <przemyslaw.wichula@gmail.com>
2020-12-06 14:19:02 +0100

    set main view and displaying planets-table add imports, declarations and provider to app.module
    HTML:
    Add planets-table selector in app component
    
    SCSS:
    add styling - table is centered and has padding - displaying table on full width in future for small devices

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {PlanetsResponse} from '../models/PlanetsResponse';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Planets';
+export class AppComponent implements OnInit {
+  currentResponse: PlanetsResponse;
+  currentPage = 1;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {PlanetsService} from '../services/planets.service';
+import {HttpClientModule} from '@angular/common/http';
+import {MatSliderModule} from '@angular/material/slider';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PlanetsTableComponent } from './planets-table/planets-table.component';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatTableModule} from '@angular/material/table';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlanetsTableComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    MatSliderModule,
+    BrowserAnimationsModule,
+    MatPaginatorModule,
+    MatTableModule
   ],
-  providers: [],
+  providers: [PlanetsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
